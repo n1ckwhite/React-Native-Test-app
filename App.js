72d7c4f1683fbc9eac03c67c22ceff263dba4f7c
Nick White <bbycinka@yandex.ru>
2023-01-28 18:06:57 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [state, setState] = useState(false)
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{state === true ? "Кнопку выключи" : "Кнопку включи"}</Text>
+            <StatusBar style="auto"/>
+            <Button
+                onPress={() => {
+                    console.log('click')
+                    setState((v) => !v)
+                }
+                }
+                title={state === true ? "Active" : "NO-active"}
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
+        </View>
+    );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: "#841584",
+        textAlign: "center",
+        fontSize: '32px',
+        textTransform: 'uppercase',
+    }
 });
